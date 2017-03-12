b59a3fe5163d25787d929504cbdc546f75c40db7
andimeo <andimeo@foxmail.com>
2017-03-12 18:17:22 +0800

    add static resources support

---
+++ werewolf/src/test/java/one/night/ultimate/WebConfig.java
@@ -0,0 +1,11 @@
+package one.night.ultimate;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+}
